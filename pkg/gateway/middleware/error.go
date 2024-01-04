a367c8839f8560a55e16d04162c2ff8946e41ce4
afifurrohman-id <afifurrohman.id@outlook.com>
2024-01-04 12:25:36 +0700

    fix: Error response from proxy

--- pkg/gateway/middleware/error.go
+++ pkg/gateway/middleware/error.go
@@ -1,33 +1,11 @@
 package middleware
 
 import (
-	"errors"
-	"fmt"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
 )
 
 func CatchServerError(ctx *fiber.Ctx, err error) error {
-	fiberError := new(fiber.Error)
-	if errors.As(err, &fiberError) {
-		log.Error("Fiber Error - ", fiberError)
-
-		if fiberError.Code == fiber.StatusNotFound {
-			return ctx.Status(fiberError.Code).Render("pages/error", map[string]any{
-				"title":   fmt.Sprintf("Error - %d", fiberError.Code),
-				"message": fmt.Sprintf("Page for Path: %s Is Not Found", ctx.Path()),
-			})
-		}
-		return ctx.Status(fiberError.Code).Render("pages/error", map[string]any{
-			"title":   fmt.Sprintf("Error - %d", fiberError.Code),
-			"message": fiberError.Code,
-		})
-	}
-
 	log.Error("Server Error - ", err)
-	return ctx.Status(fiber.StatusInternalServerError).Render("pages/error", map[string]any{
-		"title":   fmt.Sprintf("Error - %d", fiber.StatusInternalServerError),
-		"message": "Internal Server Error",
-	})
+	return ctx.Status(fiber.StatusInternalServerError).SendString("Sorry, something went wrong!, we working to fix it")
 }
